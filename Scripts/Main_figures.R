eab46f93b17a7dea1cdcfe84297fa12296db6116
Alyssa Willson <awillso2@nd.edu>
2022-02-10 20:25:03 -0500

    Moved script & created final figure script

---
+++ Scripts/Main_figures.R
@@ -0,0 +1,10 @@
+## This script contains the final plots used to visualize the data
+## at each curriculum level
+## The figure numbers correspond to the figure numbers of the manuscript
+## Supplementary figures are compiled in a separate document
+
+## Authors: AM Willson & H Gallo
+## Date modified: 10 February 2022
+
+#### Figure 3 ####
+
